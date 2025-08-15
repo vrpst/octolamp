81735aeec0602a3c4dabe5b583003e18c8a6c377
vrpst <lucas.verpoest@gmail.com>
2025-08-15 18:29:43 +0100

    add coloring yay

--- main.js
+++ main.js
@@ -8,10 +8,14 @@ import Stroke from 'ol/style/Stroke.js';
 import Style from 'ol/style/Style.js';
 import Fill from 'ol/style/Fill';
 import Select from 'ol/interaction/Select.js';
+import { createStyleFunction } from 'ol/Feature';
 
 const geojson = await fetch('./wards2.geojson')
 const geojsonObject = await geojson.json()
-console.log(geojsonObject)
+
+
+const resultsjson = await fetch('./2022-simplified.json')
+const resultsjsonObject = await resultsjson.json()
 
 const vectorSource = new VectorSource({
   features: new GeoJSON().readFeatures(geojsonObject),
@@ -19,7 +23,6 @@ const vectorSource = new VectorSource({
 
 const selected = new Style({
   stroke: new Stroke({
-    color: [70,70,70],
     width: 1,
   }),
   fill: new Fill({
@@ -31,18 +34,41 @@ const selectClick = new Select({
   style: selected,
 });
 
-const vectorLayer = new VectorLayer({
-  source: vectorSource,
-  style: new Style({
+let styleFunction = function(feature, resolution) {
+  let code = feature.get("WD23CD")
+  return new Style({
     stroke: new Stroke({
       color: [70,70,70],
       width: 1,
     }),
     fill: new Fill({
-      color: [240,240,240],
+      color: getColorToUse(resultsjsonObject[code]), //styleFunction
     })
-  }),
+  })
+}
+
+function getColorToUse(results) {
+  if (results) {
+    console.log(colors[results["control"]])
+    if (colors[results["control"]]) {
+      return colors[results["control"]]
+    } else {
+      return [137,81,41]
+    }
+  } else {
+    return [220,220,220]
+  }
+}
+
+const vectorLayer = new VectorLayer({
+  source: vectorSource,
+  style: styleFunction,
 });
+
+vectorLayer.getSource().on('addfeature', function (event) {
+  const feature = event.feature;
+  console.log(feature);
+})
 
 const map = new Map({
   target: 'map',
@@ -52,6 +78,7 @@ const map = new Map({
     zoom: 7.1,
   }),
 });
+
 
 map.addInteraction(selectClick)
 
@@ -68,3 +95,14 @@ map.on('click', async function (evt) {
 
   //local area = LAD23NM
 });
+
+const colors = {
+  LAB: [228,0,59],
+  CON: [0,135,220],
+  LD: [253,187,48],
+  GREEN: [0,116,95],
+  REF: "aqua",
+  MIX: "purple",
+  PC: [63,132,40],
+  IND: [255, 5, 241]
+}
---
+++ tools/jsonbuild.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import json
+
+df = pd.read_csv('local-elections-candidates-2022.csv')
+data = {}
+
+for i in range(18480):
+    ward_code = df.loc[i]['Ward code']
+    if ward_code not in data:
+        data[ward_code] = {'name': df.loc[i]['Ward name'], 'control': '', 'parties': {}}
+    if df.loc[i]['Elected'] == 1:
+        if df.loc[i]['Party group'] in data[ward_code]:
+            data[ward_code]['parties'][df.loc[i]['Party group']] += 1
+        else:
+            data[ward_code]['parties'][df.loc[i]['Party group']] = 1
+        #print(f"{df.loc[i]['Ward name']}\t\t\t{df.loc[i]['Ward code']}\t\t\t{df.loc[i]['Party name']}")
+
+for j in data.keys():
+    if len(data[j]['parties']) == 1:
+        print(list(data[j]['parties'].keys())[0])
+        data[j]['control'] = list(data[j]['parties'].keys())[0]
+    else:
+        data[j]['control'] = 'MIX'
+#print(data)
+
+with open('2022-simplified.json', 'a') as f:  # thank you stack overflow
+    f.write(json.dumps(data, ensure_ascii=True))
