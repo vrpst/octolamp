27fb889297fc143d00550fd21321b2ca18fb0e84
vrpst <lucas.verpoest@gmail.com>
2025-09-23 13:05:00 +0100

    add lad json data creation

--- tools/create-lad-json.py
+++ tools/create-lad-json.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import json
 
-years = ["2023", "2022", "2021", "2018", "2017", "2016"]
+
+years = ["2025", "2023", "2022", "2021", "2018", "2017", "2016"]
 for year in years:
     print(year)
     df = pd.read_csv(f'./csvs/lads/{year}.csv')
@@ -13,7 +14,6 @@ for year in years:
             data[code]["seats"].append(int(df.loc[i][name]))
 
     for i in range(len(df)):
-        print(df.loc[i]['CODE'])
         code = df.loc[i]['CODE']
         data[code] = {
             "total": int(df.loc[i]['TOTAL']),
@@ -30,7 +30,42 @@ for year in years:
             data[code]['control'] = data[code]['parties'][data[code]['seats'].index(max(data[code]['seats']))]
         else:
             data[code]['control'] = "NOC"
+        data[code]['election'] = year
     with open(f'./data/{year}/{year}-lads.json', 'w') as f:  # thank you stack overflow
         f.write(json.dumps(data, ensure_ascii=True))
 
-# DO CHANGES, 2024, 2019
+flips = years.copy()
+flips.reverse()
+flips = flips[:-1]
+results = {}
+for i in years:
+    areas = {}
+    for j in flips:
+        print("CHECKING", j, "FOR", i)
+        with open(f'./data/{j}/{j}-lads.json') as f:
+            g = json.load(f)
+            for k in g:
+                results[k] = {}
+                results[k]['prev_up'] = j
+                results[k]['prev_control'] = g[k]['control']
+    flips = flips[:-1]
+    year_lads = []
+    flip_lads = {}
+    with open(f'./data/{i}/{i}-lads.json') as x:
+        y = json.load(x)
+        for n in y:
+            year_lads.append(n)
+            flip_lads[n] = y[n]['control']
+    print("WRITING", i)
+    for m in year_lads:
+        y[m]['prev_up'] = results[m]['prev_up']
+        y[m]['prev_control'] = results[m]['prev_control']
+        if results[m]['prev_control'] != flip_lads[m]:
+            y[m]['flip'] = "true"
+        else:
+            y[m]['flip'] = "false"
+    print("WRITTEN", i)
+    with open(f'./data/{i}/{i}-lads.json', "w") as x:
+        x.write(json.dumps(y, ensure_ascii=True))
+
+    print("")
